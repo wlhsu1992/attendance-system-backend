cf378f619e00871db22a6a3d00e08a41c1d714f6
willhsu <wlhsu1992@hotmail.com>
2025-11-25 10:56:43 +0800

    Setup docker and database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/attendance-db'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
